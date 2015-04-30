9fab6c0693325212336bba37515b574c4d48fcf4
Wachira Mureithi <wachira@consultant.com>
2015-05-01 01:32:23 +0300

    First Commit

---
+++ number_limits.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<float.h>
+#include<limits.h>
+
+int main()
+{
+printf("/**************************************************************************\n *                   Mureithi David Wachira                               *\n *                   P15/2204/2011                                        *\n *                                                                        *\n *                   University of Nairobi                                *\n *                   School of Computing & Informatics                    *\n *                                                                        *\n *                   Date: Tueday 12th February 2013                      *\n *                                                                        *\n *                   CSC121 Programming & Problem Solving                 *\n *                                                                        *\n *                   Program that gets the range of numbers               *\n *                   for the various data types in C                      *\n *                                                                        *\n *                                                                        *\n *************************************************************************/\n\n");
+
+
+printf("Int max =  %d\n", INT_MAX);
+printf("Int min = %d\n\n", INT_MIN);
+
+printf("Short int max =  %d\n", SHRT_MAX);
+printf("Short int min = %d\n\n", SHRT_MIN);
+
+printf("Long max =  %d\n", LONG_MAX);
+printf("Long min = %d\n\n", LONG_MIN);
+
+printf("Double max = %g\n", DBL_MAX);
+printf("Double min = %g\n\n", DBL_MIN);
+
+printf("Char max = %g\n", CHAR_MAX);
+printf("Char min = %g\n\n", CHAR_MIN);
+
+printf("Float max = %g\n", FLT_MAX);
+printf("Float min = %g\n\n", FLT_MIN);
+
+return 0;
+}
